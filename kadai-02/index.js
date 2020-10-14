f53c2e060cc89d8e62eeadbe155bfbbe706c037a
takenakatakumi <gh8302p.fge@gmail.com>
2020-10-14 05:28:58 +0000

    課題2を追加

---
+++ kadai-02/index.js
@@ -0,0 +1,18 @@
+// 文字列を結合して表示する
+const firstname = 'Takumi'
+const familyname = 'Takenaka'
+console.log(firstname + familyname)
+
+// 1から10までの数字の和を表示する
+console.log(1+2+3+4+5+6+7+8+9+10)
+
+// 真偽型の演算
+const dog = false
+const cat = false
+// 犬または猫を飼っている
+console.log(dog || cat)
+// 犬と猫の両方を飼っている
+console.log(dog && cat)
+// 犬も猫も飼っていない
+console.log(!dog && !cat)
+console.log(!(dog || cat))
